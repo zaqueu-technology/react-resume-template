a2846c9c4c1c041ca3839ada1eb75c2d303a1433
Zaqueu <zaqueugithub@gmail.com>
2024-11-06 17:38:20 -0300

    made my portfolio

--- src/components/Sections/Contact/ContactForm.tsx
+++ src/components/Sections/Contact/ContactForm.tsx
@@ -2,7 +2,6 @@ import {FC, memo, useCallback, useMemo, useState} from 'react';
 
 interface FormData {
   name: string;
-  email: string;
   message: string;
 }
 
@@ -10,7 +9,6 @@ const ContactForm: FC = memo(() => {
   const defaultData = useMemo(
     () => ({
       name: '',
-      email: '',
       message: '',
     }),
     [],
@@ -21,54 +19,44 @@ const ContactForm: FC = memo(() => {
   const onChange = useCallback(
     <T extends HTMLInputElement | HTMLTextAreaElement>(event: React.ChangeEvent<T>): void => {
       const {name, value} = event.target;
-
-      const fieldData: Partial<FormData> = {[name]: value};
-
-      setData({...data, ...fieldData});
+      setData(prevData => ({...prevData, [name]: value}));
     },
-    [data],
+    [],
   );
 
-  const handleSendMessage = useCallback(
-    async (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-      /**
-       * This is a good starting point to wire up your form submission logic
-       * */
-      console.log('Data to send: ', data);
-    },
-    [data],
-  );
+  const handleSendMessage = useCallback(() => {
+    const whatsappNumber = '5584991458984'; // Substitua pelo seu número completo, incluindo o código do país e da área.
+    const message = `Olá, meu nome é ${data.name}. ${data.message}`;
+
+    // Criar o link para WhatsApp com o texto da mensagem
+    const whatsappLink = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(message)}`;
+
+    // Redireciona para o WhatsApp
+    window.location.href = whatsappLink;
+  }, [data]);
 
   const inputClasses =
     'bg-neutral-700 border-0 focus:border-0 focus:outline-none focus:ring-1 focus:ring-orange-600 rounded-md placeholder:text-neutral-400 placeholder:text-sm text-neutral-200 text-sm';
 
   return (
-    <form className="grid min-h-[320px] grid-cols-1 gap-y-4" method="POST" onSubmit={handleSendMessage}>
-      <input className={inputClasses} name="name" onChange={onChange} placeholder="Name" required type="text" />
-      <input
-        autoComplete="email"
-        className={inputClasses}
-        name="email"
-        onChange={onChange}
-        placeholder="Email"
-        required
-        type="email"
-      />
+    <form className="grid min-h-[320px] grid-cols-1 gap-y-4">
+      <input className={inputClasses} name="name" onChange={onChange} placeholder="Nome" required type="text" />
       <textarea
         className={inputClasses}
         maxLength={250}
         name="message"
         onChange={onChange}
-        placeholder="Message"
+        placeholder="Mensagem"
         required
         rows={6}
       />
       <button
-        aria-label="Submit contact form"
+        aria-label="Enviar mensagem para WhatsApp"
         className="w-max rounded-full border-2 border-orange-600 bg-stone-900 px-4 py-2 text-sm font-medium text-white shadow-md outline-none hover:bg-stone-800 focus:ring-2 focus:ring-orange-600 focus:ring-offset-2 focus:ring-offset-stone-800"
-        type="submit">
-        Send Message
+        type="button"
+        onClick={handleSendMessage} // Altere o evento para onClick
+      >
+        WhatsApp
       </button>
     </form>
   );
--- src/data/data.tsx
+++ src/data/data.tsx
@@ -14,8 +14,8 @@ import LinkedInIcon from '../components/Icon/LinkedInIcon';
 import StackOverflowIcon from '../components/Icon/StackOverflowIcon';
 import TwitterIcon from '../components/Icon/TwitterIcon';
 import heroImage from '../images/header-background.webp';
-import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
-import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
+import porfolioImage1 from '../images/portfolio/portfolio-1.png';
+import porfolioImage2 from '../images/portfolio/portfolio-2.png';
 import porfolioImage3 from '../images/portfolio/portfolio-3.jpg';
 import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
 import porfolioImage5 from '../images/portfolio/portfolio-5.jpg';
@@ -44,8 +44,8 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'React Resume Template',
-  description: "Example site built with Tim Baker's react resume template",
+  title: "Zaqueu's resume",
+  description: 'My resume',
 };
 
 /**
@@ -53,13 +53,13 @@ export const homePageMeta: HomepageMeta = {
  */
 export const SectionId = {
   Hero: 'hero',
-  About: 'about',
-  Contact: 'contact',
+  About: 'sobre',
+  Contact: 'contato',
   Portfolio: 'portfolio',
   Resume: 'resume',
-  Skills: 'skills',
+  Skills: 'habilidades',
   Stats: 'stats',
-  Testimonials: 'testimonials',
+  Testimonials: 'frases',
 } as const;
 
 export type SectionId = (typeof SectionId)[keyof typeof SectionId];
@@ -69,18 +69,16 @@ export type SectionId = (typeof SectionId)[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Tim Baker.`,
+  name: `Eu sou Zaqueu.`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Victoria based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
-        at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
-        registrar and site builder.
+        Sou um estudante de <strong className="text-stone-100">Programação e Eletrônica</strong> baseado em Mossoró/RN.
+        Atualmente foco em estudar desenvolvimento web <strong className="text-stone-100">Full Stack</strong>
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
-        plucking my <strong className="text-stone-100">banjo</strong>, or exploring beautiful{' '}
-        <strong className="text-stone-100">Vancouver Island</strong>.
+        No meu tempo livre eu sou{' '}
+        <strong className="text-stone-100">Músico, Escritor e pratico atividades físicas.</strong>
       </p>
     </>
   ),
@@ -93,7 +91,7 @@ export const heroData: Hero = {
     },
     {
       href: `#${SectionId.Contact}`,
-      text: 'Contact',
+      text: 'Contato',
       primary: false,
     },
   ],
@@ -104,16 +102,24 @@ export const heroData: Hero = {
  */
 export const aboutData: About = {
   profileImageSrc: profilepic,
-  description: `Use this bio section as your way of describing yourself and saying what you do, what technologies you like
-  to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing
-  in.`,
+  description: `Adoro o conceito de open source e acredito que a criação coletiva e a liberdade criativa são os pilares para o avanço da tecnologia. Tenho um interesse especial em desenvolvimento web, e minha jornada tem sido movida pela curiosidade de entender o que há por trás das telas e como criar experiências simples e intuitivas.
+
+Além de programar, sou um grande fã de música e toco violão nas horas vagas – até estou pensando em gravar um álbum lo-fi com algumas músicas que escrevi! Meus objetivos para o futuro incluem trabalhar com equipes internacionais e ganhar experiência prática em projetos reais, onde eu possa aprender com profissionais mais experientes e contribuir para algo significativo.`,
   aboutItems: [
-    {label: 'Location', text: 'Victoria, BC', Icon: MapIcon},
-    {label: 'Age', text: '29', Icon: CalendarIcon},
-    {label: 'Nationality', text: 'Canadian / Irish', Icon: FlagIcon},
-    {label: 'Interests', text: 'Motorcycles, Muay Thai, Banjos', Icon: SparklesIcon},
-    {label: 'Study', text: 'University of Victoria', Icon: AcademicCapIcon},
-    {label: 'Employment', text: 'Instant Domains, inc.', Icon: BuildingOffice2Icon},
+    {label: 'Localização', text: 'Mossoró, RN', Icon: MapIcon},
+    {label: 'Idade', text: '18', Icon: CalendarIcon},
+    {label: 'Nacionalidade', text: 'Brasileiro', Icon: FlagIcon},
+    {
+      label: 'Interesses',
+      text: 'Criação musical, Programação web, Design, Escrita, Atividade física',
+      Icon: SparklesIcon,
+    },
+    {
+      label: 'Estudo',
+      text: 'Instituto Federal de Educação, Ciência e Tecnologia do Rio Grande do Norte(IFRN) - Campus Mossoró',
+      Icon: AcademicCapIcon,
+    },
+    /* {label: 'Employment', text: 'Instant Domains, inc.', Icon: BuildingOffice2Icon}, */
   ],
 };
 
@@ -122,71 +128,79 @@ export const aboutData: About = {
  */
 export const skills: SkillGroup[] = [
   {
-    name: 'Spoken languages',
+    name: 'Línguas faladas',
     skills: [
       {
-        name: 'English',
-        level: 10,
+        name: 'Inglês',
+        level: 8,
       },
       {
-        name: 'French',
+        name: 'Francês',
         level: 4,
       },
       {
-        name: 'Spanish',
+        name: 'Espanhol',
         level: 3,
       },
     ],
   },
   {
-    name: 'Frontend development',
+    name: 'Desenvolvimento Front-End',
     skills: [
       {
         name: 'React',
-        level: 9,
-      },
-      {
-        name: 'Typescript',
-        level: 7,
-      },
-      {
-        name: 'GraphQL',
-        level: 6,
-      },
-    ],
-  },
-  {
-    name: 'Backend development',
-    skills: [
-      {
-        name: 'Node.js',
-        level: 8,
-      },
-      {
-        name: 'Rust',
         level: 5,
       },
       {
-        name: 'Golang',
+        name: 'Javascript',
+        level: 5,
+      },
+      {
+        name: 'Tailwind CSS',
+        level: 5,
+      },
+      {
+        name: 'Bootstrap',
+        level: 3,
+      },
+      {
+        name: 'Material UI',
         level: 4,
       },
     ],
   },
   {
-    name: 'Mobile development',
+    name: 'Desenvolvimento Back-End',
     skills: [
       {
-        name: 'React Native',
-        level: 9,
+        name: 'Node.js',
+        level: 3,
       },
       {
+        name: 'Java',
+        level: 5,
+      },
+      /* {
+        name: 'Golang',
+        level: 4,
+      }, */
+    ],
+  },
+  {
+    name: 'Desenvolvimento Android',
+    skills: [
+      {
+        name: 'Apache Cordova',
+        level: 6,
+      },
+      /* {
         name: 'Flutter',
         level: 4,
       },
       {
         name: 'Swift',
         level: 3,
-      },
+      }, */
     ],
   },
 ];
@@ -196,17 +210,20 @@ export const skills: SkillGroup[] = [
  */
 export const portfolioItems: PortfolioItem[] = [
   {
-    title: 'Project title 1',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: 'Catálogo de produtos e site pessoal',
+    description:
+      'Para esse projeto, usei React e Tailwind para criar um site de catálogo de produtos, visando facilitar o acesso e aprimorar a experiência do cliente.',
+    url: 'https://elaniamodaintima.vercel.app/Sobre',
     image: porfolioImage1,
   },
   {
-    title: 'Project title 2',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: 'Suap Notas',
+    description:
+      'Nesse projeto, usei Javascript e Apache Cordova para criar um aplicativo que calcula a média semestral e anual do sistema SUAP, utilizado no IFRN Campus Mossoró.',
+    url: '',
     image: porfolioImage2,
   },
+  /*
   {
     title: 'Project title 3',
     description: 'Give a short description of your project here.',
@@ -260,7 +277,7 @@ export const portfolioItems: PortfolioItem[] = [
     description: 'Give a short description of your project here.',
     url: 'https://reactresume.com',
     image: porfolioImage11,
-  },
+  }, */
 ];
 
 /**
@@ -268,32 +285,40 @@ export const portfolioItems: PortfolioItem[] = [
  */
 export const education: TimelineItem[] = [
   {
-    date: 'April 2007',
-    location: 'Clown college',
-    title: 'Masters in Beer tasting',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    date: 'Março 2021 - Presente',
+    location: 'IFRN - Campus Mossoró',
+    title: 'Ensino médio',
+    content: (
+      <p>
+        No IFRN, estou desenvolvendo uma sólida base em fundamentos técnicos e analíticos através do curso de
+        Eletrotécnica. Esse programa de formação técnica me permite aprender sobre circuitos elétricos, automação, e
+        sistemas de potência, além de trabalhar em projetos práticos que exigem habilidades de resolução de problemas e
+        pensamento lógico – competências que são cruciais tanto na eletrotécnica quanto na programação.
+      </p>
+    ),
   },
-  {
+  /* {
     date: 'March 2003',
     location: 'School of Business',
     title: 'What did you study 101',
     content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
-  },
+  }, */
 ];
 
 export const experience: TimelineItem[] = [
   {
-    date: 'March 2010 - Present',
-    location: 'Awesome Development Company',
-    title: 'Senior UX Engineer',
+    date: '2021 - Presente',
+    location: '',
+    title: 'Desenvolvedor Freelancer',
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+        Sem experiência prévia em empresas. Essa é uma oportunidade única para você ser o primeiro a me contratar :D
+        <br></br>
+        Até então, fiz alguns projetos para uma loja e para o IFRN.
       </p>
     ),
   },
-  {
+  /* {
     date: 'March 2007 - February 2010',
     location: 'Garage Startup Studio',
     title: 'Junior bug fixer',
@@ -303,7 +328,7 @@ export const experience: TimelineItem[] = [
         anything else that would be useful for an employer to know.
       </p>
     ),
-  },
+  }, */
 ];
 
 /**
@@ -313,19 +338,20 @@ export const testimonial: TestimonialSection = {
   imageSrc: testimonialImage,
   testimonials: [
     {
-      name: 'John Doe',
-      text: 'Use this as an opportunity to promote what it is like to work with you. High value testimonials include ones from current or past co-workers, managers, or from happy clients.',
-      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/169.jpg',
+      name: 'Epitecto',
+      text: 'Querias ser livre. Para essa liberdade, só há um caminho: o desprezo das coisas que não dependem de nós.',
+      image: 'https://www.edipro.com.br/wp-content/uploads/2021/10/epicteto.png',
     },
     {
-      name: 'Jane Doe',
-      text: 'Here you should write some nice things that someone has said about you. Encourage them to be specific and include important details (notes about a project you were on together, impressive quality produced, etc).',
-      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/14.jpg',
+      name: 'Sêneca',
+      text: 'Apressa-te a viver bem e pensa que cada dia é, por si só, uma vida.',
+      image:
+        'https://midias.correiobraziliense.com.br/_midias/png/2022/11/02/675x450/1_seneca-26775398.png?20221102141552?20221102141552',
     },
     {
-      name: 'Someone else',
-      text: 'Add several of these, and keep them as fresh as possible, but be sure to focus on quality testimonials with strong highlights of your skills/work ethic.',
-      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/69.jpg',
+      name: 'Marco Aurélio',
+      text: 'Se você está sofrendo por coisas externas, não são elas que estão te perturbando, mas o seu próprio julgamento sobre elas. E está em seu poder anular este julgamento agora.',
+      image: 'https://th.bing.com/th/id/R.b93363e78ddff767e2a719d7feedbe7f?rik=gQVaQ%2bGa2YnX%2fQ&pid=ImgRaw&r=0',
     },
   ],
 };
@@ -335,28 +361,33 @@ export const testimonial: TestimonialSection = {
  */
 
 export const contact: ContactSection = {
-  headerText: 'Get in touch.',
-  description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
+  headerText: 'Entre em contato.',
+  description: 'Entre em contato comigo por Email, WhatsApp, ou o que achar melhor.',
   items: [
     {
       type: ContactType.Email,
-      text: 'reachout@timbaker.me',
-      href: 'mailto:reachout@timbaker.me',
+      text: 'zaqueunilton1@gmail.com',
+      href: 'mailto:zaqueunilton1@gmail.com',
     },
     {
       type: ContactType.Location,
-      text: 'Victoria BC, Canada',
-      href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
+      text: 'Mossoró, RN',
+      href: '',
     },
-    {
+    /* {
       type: ContactType.Instagram,
       text: '@tbakerx',
       href: 'https://www.instagram.com/tbakerx/',
-    },
+    }, */
     {
       type: ContactType.Github,
-      text: 'tbakerx',
-      href: 'https://github.com/tbakerx',
+      text: 'zaqueu-technology',
+      href: 'https://github.com/zaqueu-technology',
+    },
+    {
+      type: ContactType.Phone,
+      text: '(84) 9 9145-8984',
+      href: 'https://wa.me/5584991458984',
     },
   ],
 };
@@ -365,9 +396,9 @@ export const contact: ContactSection = {
  * Social items
  */
 export const socialLinks: Social[] = [
-  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/tbakerx'},
-  {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},
-  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/timbakerx/'},
-  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/reactresume/'},
-  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/TimBakerx'},
+  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/zaqueu-technology'},
+  /* {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'}, */
+  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/zaqueunilton/'},
+  /* {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/reactresume/'}, */
+  /* {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/TimBakerx'}, */
 ];
